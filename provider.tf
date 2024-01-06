a0f82d2c3d5c88de073cb97fbda3242de9d07937
HM <hafeez.itm@gmail.com>
2024-01-06 12:58:08 -0500

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,30 @@
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~>3.0"
+#     }
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "~>3.0"
+#     }
+#   }
+# }
+# provider "azurerm" {
+#     features {}
+# }
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "=3.85.0"
     }
   }
 }
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-    features {}
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
 }
